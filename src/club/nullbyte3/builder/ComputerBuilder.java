35bb99e4e7b8418be6c99d4615fc962ee2f07db1
Your Name <you@example.com>
2025-09-25 13:09:01 +0300

    wip: builder exampole

---
+++ src/club/nullbyte3/builder/ComputerBuilder.java
@@ -0,0 +1,10 @@
+package club.nullbyte3.builder;
+
+public interface ComputerBuilder {
+    void buildProcessor();
+    void buildRAM();
+    void buildHardDrive();
+    void buildGraphicsCard();
+    void buildOperatingSystem();
+    Computer getComputer();
+}
